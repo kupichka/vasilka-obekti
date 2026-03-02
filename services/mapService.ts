2b218a72d78587de900d5101d3b42149653ac545
100daysummer <bobbydochev@gmail.com>
2026-03-02 02:18:01 +0200

    debug

--- services/mapService.ts
+++ services/mapService.ts
@@ -154,7 +154,7 @@ class MapService {
 
     const vtTile = this.tileIndex.getTile(coords.z, coords.x, coords.y);
     if (!vtTile) {
-      console.log("exiting the drawTileCanvas function... at point B");
+      console.log("exiting the drawTileCanvas function... at point BAAA");
       ctx.restore();
       return;
     }
